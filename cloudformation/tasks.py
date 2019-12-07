f09abd722f049788fd462689e6ba1ddfebd56c3f
Rob Kenis <rob.kenis@hotmail.com>
2019-12-06 20:50:59 -0800

    Add invoke script to deploy stack

--- cloudformation/tasks.py
+++ cloudformation/tasks.py
@@ -1,8 +1,11 @@
 import os
 
+import boto3
 from invoke import task
 
 CLOUDFORMATION_TEMPLATES_DIR = 'templates'
+
+client = boto3.client('cloudformation')
 
 
 @task
@@ -12,3 +15,13 @@ def build(c, docs=False):
         os.system("python {base}/{file}".
                   format(base=CLOUDFORMATION_TEMPLATES_DIR, file=filename))
         print("\t-> Finished building {file}".format(file=filename))
+
+
+@task
+def deploy(c, docs=False):
+    with open('output/frontend_hosting.json', 'r') as template_body:
+        client.update_stack(
+            StackName='spunt-punt-be',
+            TemplateBody=template_body.read(),
+            Capabilities=['CAPABILITY_IAM'],
+        )

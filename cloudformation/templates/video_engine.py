6d3a37e99ea98aee0e56551a27f7b5333193aeaa
Rob Kenis <rob.kenis@vrt.be>
2019-12-14 15:14:00 +0100

    #8: Send notifications about new object to SQS
    Queuing is nice.

--- cloudformation/templates/video_engine.py
+++ cloudformation/templates/video_engine.py
@@ -1,14 +1,37 @@
-from troposphere import Template, Ref, Join, AWS_STACK_NAME
+from troposphere import Template, Ref, Join, AWS_STACK_NAME, GetAtt
 from troposphere.s3 import Bucket, NotificationConfiguration, TopicConfigurations, Filter, S3Key, Rules
-from troposphere.sns import Topic, TopicPolicy
+from troposphere.sns import Topic, TopicPolicy, Subscription
+from troposphere.sqs import Queue, QueuePolicy
 
 template = Template(Description='Video engine for spunt.be')
 
 _upload_bucket_name = Join('-', [Ref(AWS_STACK_NAME), 'upload'])
 
+encode_video_queue = template.add_resource(Queue(
+    'EncodeVideoQueue'
+))
+
 upload_topic = template.add_resource(Topic(
     'UploadTopic',
+    Subscription=[Subscription(
+        Endpoint=GetAtt(encode_video_queue, 'Arn'),
+        Protocol='SQS',
+    )],
+))
 
+template.add_resource(QueuePolicy(
+    "EncodeVideoQueuePolicy",
+    Queues=[Ref(encode_video_queue)],
+    PolicyDocument={
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Action": ["sqs:SendMessage"],
+            "Resource": GetAtt(encode_video_queue, 'Arn'),
+            "Principal": {"Service": "sns.amazonaws.com"},
+            "Condition": {"ArnEquals": {"aws:SourceArn": Ref(upload_topic)}},
+        }],
+    },
 ))
 
 template.add_resource(TopicPolicy(

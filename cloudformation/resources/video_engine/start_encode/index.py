1919d1585b41465035645b64d3326d6db7ab9af3
Rob Kenis <rob.kenis@vrt.be>
2019-12-15 12:13:14 +0100

    #8: Return list of videoIds after saving to dynamo
    These ids will be sent to the destination and used for further
    processing of the video.

--- cloudformation/resources/video_engine/start_encode/index.py
+++ cloudformation/resources/video_engine/start_encode/index.py
@@ -11,15 +11,17 @@ dynamo = boto3.client('dynamodb')
 
 
 def handler(event, context):
+    video_ids = []
     for record in event['Records']:
         print("Handling message with id: [{eventId}]".format(eventId=record['messageId']))
         sns_msg = json.loads(record['body'])
         msg = json.loads(sns_msg['Message'])
         for notification in msg['Records']:
+            video_id = str(uuid.uuid1())
             dynamo.put_item(
                 TableName=VIDEO_EVENTS_TABLE,
                 Item={
-                    'videoId': {'S': str(uuid.uuid1())},
+                    'videoId': {'S': video_id},
                     'type': {'S': 'NEW_VIDEO_UPLOADED'},
                     'timestamp': {'S': datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()},
                     'metadata': {'S': json.dumps({
@@ -29,7 +31,10 @@ def handler(event, context):
                 }
             )
             print("NEW_VIDEO_UPLOADED event for [{path}] saved.".format(path=notification['s3']['object']['key']))
+            video_ids.append(video_id)
     return {
         'statusCode': 200,
-        'body': json.dumps({'message': 'Events handled.'})
+        'body': json.dumps({
+            'messages': list(map(lambda x: {'videoId': x, 'status': 'NEW_VIDEO_UPLOADED'}, video_ids))
+        })
     }

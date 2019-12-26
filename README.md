6cbb139c56eec1b50105fe2d52f4c4ea33b02f12
Rob Kenis <rob.kenis@vrt.be>
2019-12-26 19:48:12 +0100

    Implement lamba and add some readme fluff

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Spunt.be
+
+## Video Engine
+The video engine handles uploaded videos in the `upload-s3-bucket` and encodes these files to a supported format by the THEOplayer.
+For this case, we've picked MPEG-DASH.
+### The flow
+1. The uploaded file in S3 triggeres a notification to SNS
+2. SNS forwards the message to a lambda to start the encode. This lambda creates an event in the `video-events-table`. 
+After the lambda has executed, the resulting body is sent to the success-destination.
+3. The success destination (SNS in this case) triggers the `request-encoding-lambda`. This lambda creates a job at
+the Elastic Transcoder Pipeline to encode the `mp4` to `mpd`.
+4. Elastic Transcoder sends updates to an SNS topic, the `update-state-lambda` is subscribed to this topic. When an
+event is consumed, the state from the SNS message is saved as an event in the `video-events-table`.
+5. That's all, the video is now available to be played.
+#### Supported events
+- NEW_VIDEO_UPLOADED
+- ENCODING_REQUESTED
+- ENCODING_STARTED
+- ENCODING_FAILED
+- ENCODING_COMPLETED
--- cloudformation/resources/video_engine/update_encoding_state/index.py
+++ cloudformation/resources/video_engine/update_encoding_state/index.py
@@ -1,5 +1,50 @@
+import datetime
 import json
+import os
+
+import boto3
+
+VIDEO_EVENTS_TABLE = os.environ.get('VIDEO_EVENTS_TABLE')
+
+dynamo = boto3.client('dynamodb')
+
+
+def _store_event(video_id, status, metadata):
+    dynamo.put_item(
+        TableName=VIDEO_EVENTS_TABLE,
+        Item={
+            'videoId': {'S': video_id},
+            'type': {'S': status},
+            'timestamp': {'S': datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()},
+            'metadata': {'S': json.dumps(metadata)}
+        }
+    )
+
+
+def _encoding_started(video_id):
+    _store_event(video_id, 'ENCODING_STARTED', {})
+
+
+def _encoding_failed(video_id):
+    _store_event(video_id, 'ENCODING_FAILED', {})
+
+
+def _encoding_completed(video_id):
+    _store_event(video_id, 'ENCODING_COMPLETED', {'path': "{videoId}/{videoId}.mpd".format(videoId=video_id)})
 
 
 def handler(event, context):
-    print(json.dumps(event))
+    for record in event['Records']:
+        print("Handling message with id: [{eventId}]".format(eventId=record['Sns']['MessageId']))
+        msg = json.loads(record['Sns']['Message'])
+        print(msg)
+        video_id = msg['userMetadata']['videoId']
+        state = msg['state']
+        print("Video with id: [{videoId}] is in state: [{state}]".format(videoId=video_id, state=state))
+        switch = {
+            'ERROR': _encoding_failed,
+            'PROGRESSING': _encoding_started,
+            'COMPLETED': _encoding_completed
+        }
+        state_handler = switch.get(state, lambda: "Invalid state")
+        state_handler(video_id)

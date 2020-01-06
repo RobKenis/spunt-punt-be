2ada88b7ce676b71e4541bfa0511a7de9a56cae3
Rob Kenis <rob.kenis@vrt.be>
2020-01-06 19:10:32 +0100

    Add CORS headers

--- cloudformation/resources/spunt_api/hot_videos/index.js
+++ cloudformation/resources/spunt_api/hot_videos/index.js
@@ -3,30 +3,37 @@ const body = {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }, {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }, {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }, {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }, {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }, {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }, {
         id: '3e72bdc6-28e3-11ea-abb5-5a2040ddf892',
         title: 'Very pretty title',
         playbackUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.mpd',
+        thumbnailUrl: 'https://videos.spunt.be/3e72bdc6-28e3-11ea-abb5-5a2040ddf892/3e72bdc6-28e3-11ea-abb5-5a2040ddf892.jpg',
     }]
 };
 
@@ -35,6 +42,11 @@ exports.handler = (event, context, callback) => {
         status: '200',
         statusDescription: 'OK',
         body: JSON.stringify(body),
+        headers: {
+            'access-control-allow-origin': [{key: 'Access-Control-Allow-Origin', value: "*"}],
+            'access-control-allow-methods': [{key: 'Access-Control-Allow-Methods', value: "GET, HEAD, OPTIONS"}],
+            'access-control-max-age': [{key: 'Access-Control-Max-Age', value: "86400"}],
+        }
     };
     callback(null, response);
 };

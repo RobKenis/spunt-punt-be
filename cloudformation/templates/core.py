68bb63c2a08df3e3de12038bb54fc8044f38f321
Rob Kenis <rob.kenis@vrt.be>
2020-01-11 11:55:46 +0100

    Consume dynamoDB stream to route events to consumer

--- cloudformation/templates/core.py
+++ cloudformation/templates/core.py
@@ -1,9 +1,13 @@
-from troposphere import Template, Output, Ref, Export, Join, AWS_STACK_NAME, GetAtt
-from troposphere.dynamodb import Table, AttributeDefinition, KeySchema
-from troposphere.iam import ManagedPolicy
+from troposphere import Template, Output, Ref, Export, Join, AWS_STACK_NAME, GetAtt, iam
+from troposphere.awslambda import Environment
+from troposphere.dynamodb import Table, AttributeDefinition, KeySchema, StreamSpecification
+from troposphere.iam import ManagedPolicy, Role
+from troposphere.logs import LogGroup
 from troposphere.s3 import Bucket
+from troposphere.serverless import Function
 
 template = Template(Description='Core resources for spunt.be')
+template.set_transform('AWS::Serverless-2016-10-31')
 
 lambda_code_bucket = template.add_resource(Bucket(
     'LambdaCodeBucket',
@@ -27,6 +31,9 @@ video_events_table = template.add_resource(Table(
         AttributeName='timestamp',
         KeyType='RANGE',
     )],
+    StreamSpecification=StreamSpecification(
+        StreamViewType='NEW_IMAGE',
+    ),
 ))
 
 # Managed policies
@@ -53,6 +60,62 @@ lambda_managed_policy = template.add_resource(ManagedPolicy(
             "Effect": "Allow",
         }],
     }
+))
+
+event_router_role = template.add_resource(Role(
+    'EventRouterRole',
+    Path="/",
+    AssumeRolePolicyDocument={
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Action": ["sts:AssumeRole"],
+            "Effect": "Allow",
+            "Principal": {"Service": ["lambda.amazonaws.com"]},
+        }],
+    },
+    ManagedPolicyArns=[Ref(lambda_managed_policy)],
+    Policies=[iam.Policy(
+        PolicyName="video-event-router",
+        PolicyDocument={
+            "Version": "2012-10-17",
+            "Statement": [{
+                "Action": ["dynamodb:DescribeStream", "dynamodb:GetRecords",
+                           "dynamodb:GetShardIterator", "dynamodb:ListStreams"],
+                "Resource": [Join('', [GetAtt(video_events_table, 'Arn'), '/stream/*'])],
+                "Effect": "Allow",
+            }],
+        })],
+))
+
+with open('resources/spunt_core/event_router/index.py', 'r') as lambda_code:
+    event_router_function = template.add_resource(Function(
+        'EventRouterFunction',
+        Handler='index.handler',
+        Runtime='python3.7',
+        InlineCode=lambda_code.read(),
+        Description='Consumes events dynamoDB stream and routes events to corresponding queues.',
+        Role=GetAtt(event_router_role, 'Arn'),
+        Environment=Environment(
+            Variables={
+                'API_EVENTS_QUEUE': '_TODO_'
+            }
+        ),
+        Events={
+            'VideoEventsStream': {
+                'Type': 'DynamoDB',
+                'Properties': {
+                    'Enabled': True,
+                    'StartingPosition': 'TRIM_HORIZON',
+                    'Stream': GetAtt(video_events_table, 'StreamArn'),
+                }
+            }
+        },
+    ))
+
+template.add_resource(LogGroup(
+    "EventRouterFunctionLogGroup",
+    LogGroupName=Join('/', ['/aws/lambda', Ref(event_router_function)]),
+    RetentionInDays=7,
 ))
 
 template.add_output(Output(

517c469b2a81e219be262660038c9e8608bdccf0
Rob Kenis <rob.kenis@vrt.be>
2020-01-11 09:48:32 +0100

    Store event after Rekognition has started

--- cloudformation/resources/video_insights_engine/video_metadata_event/index.py
+++ cloudformation/resources/video_insights_engine/video_metadata_event/index.py
@@ -1,6 +1,21 @@
+import datetime
 import json
+import os
+
+import boto3
+
+VIDEO_EVENTS_TABLE = os.environ.get('VIDEO_EVENTS_TABLE')
+
+dynamo = boto3.client('dynamodb')
 
 
 def handler(event, context):
-    print(json.dumps(event))
-    return {'message': 'nice'}
+    dynamo.put_item(
+        TableName=VIDEO_EVENTS_TABLE,
+        Item={
+            'videoId': {'S': event['videoId']},
+            'type': {'S': event['status']},
+            'timestamp': {'S': datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()},
+            'metadata': {'S': json.dumps({})}
+        }
+    )

8ad94c21568e26824bbb84fb931a792a697785dd
Kenneth De Win <k.dewin@persgroep.nl>
2020-01-12 23:20:22 +0100

    Show placeholder when video thumbnail takes a while to load

--- frontend/src/components/video-preview/VideoPreview.jsx
+++ frontend/src/components/video-preview/VideoPreview.jsx
@@ -5,10 +5,15 @@ export class VideoPreview extends Component {
   render() {
     return (
       <div className="video-preview">
-        <a href={`/video/${this.props.video.id}`}>
-          <img src="https://placekitten.com/720/405" alt={this.props.video.title} />
-          <h3>{this.props.video.title}</h3>
-        </a>
+        <div className="video-preview__aspect-ratio--outer">
+          <div className="video-preview__placeholder">
+            S<span>.</span>
+          </div>
+          <a className="video-preview__aspect-ratio--inner" href={`/video/${this.props.video.videoId}`}>
+            <img src={this.props.video.thumbnailUrl} alt={this.props.video.title} />
+          </a>
+        </div>
+        <h3>{this.props.video.title}</h3>
       </div>
     );
   }

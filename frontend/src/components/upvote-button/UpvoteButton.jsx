ac09822bb189d3e8bed7f49b7a02757522a295ab
Kenneth De Win <k.dewin@persgroep.nl>
2020-01-14 21:52:41 +0100

    Style vote buttons

--- frontend/src/components/upvote-button/UpvoteButton.jsx
+++ frontend/src/components/upvote-button/UpvoteButton.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
-import { upvoteVideo } from "../../api/VideoApiClient";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { downvoteVideo } from "../../api/VideoApiClient";
 
 export class UpvoteButton extends Component {
   constructor(props, context) {
     super(props, context);
   }
 
-  upvote = () => upvoteVideo(this.props.videoId);
+  downvote() {
+    downvoteVideo(this.props.videoId).then((response) => console.log(response));
+  }
 
   render() {
-    return <button onClick={() => this.upvote()}>Upvote</button>;
+    return (
+      <button className="button is-success" onClick={this.downvote.bind(this)}>
+        <FontAwesomeIcon icon={faArrowUp} size="sm" fixedWidth />
+      </button>
+    );
   }
 }

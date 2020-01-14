01c185b9e8d90ba6137c13ac3d504feffd4abc94
Kenneth De Win <k.dewin@persgroep.nl>
2020-01-15 00:47:39 +0100

    Add labels to video previews

--- frontend/src/components/video-preview/VideoPreview.jsx
+++ frontend/src/components/video-preview/VideoPreview.jsx
@@ -22,6 +22,14 @@ export class VideoPreview extends Component {
           <h3 className="video-preview__title">
             <a href={`/video/${this.props.video.videoId}`}>{this.props.video.title}</a>
           </h3>
+          <ul className="video--preview__labels">
+            {this.props.video.labels &&
+              this.props.video.labels.slice(0, this.props.layout === LAYOUT.VERTICAL ? 3 : 2).map((label, index) => (
+                <li key={index} className="video-preview__label button is-small is-white is-outlined">
+                  {label}
+                </li>
+              ))}
+          </ul>
         </div>
       </div>
     );
